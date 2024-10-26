6498d3471f4618dd01a135fedfb26c27a31eae9e
Narongrit2007 <bakmaxnajabakmaxnaja@gmail.com>
2024-10-27 00:16:06 +0700

    Update HHHHHHHHH-dsfsaf-N---a--j--a--.lua

--- HHHHHHHHH-dsfsaf-N---a--j--a--.lua
+++ HHHHHHHHH-dsfsaf-N---a--j--a--.lua
@@ -1,82 +1,81 @@
--- Restrict access to only users with getgenv().K = "Pak1"
-if getgenv().K ~= "Pak1" then
-    print("Access Denied: Invalid Key")
-    return -- Terminates the script if the key is not "Pak1"
-end
-
--- Continue with the rest of the script
-
+--getgenv().K = "Pak1"
 function LoadSettings()
-    if readfile and writefile and isfile and isfolder then
-        if not isfolder("Key test") then
-            makefolder("Key test")
-        end
-        if not isfolder("Key test/MrMaxNaJa/") then
-            makefolder("Key test/MrMaxNaJa/")
-        end
-        if not isfile("Key test/MrMaxNaJa/?.json") then
-            writefile("Key test/MrMaxNaJa/?.json", game:GetService("HttpService"):JSONEncode(_G.Settings))
-        else
-            local L_54_ = game:GetService("HttpService"):JSONDecode(readfile("Key test/MrMaxNaJa/?.json"))
-            for L_55_forvar0, L_56_forvar1 in pairs(L_54_) do
-                _G.Settings[L_55_forvar0] = L_56_forvar1
-            end
-        end
-    else
-        return warn("Status : Undetected Executor")
-    end
+	if readfile and writefile and isfile and isfolder then
+		if not isfolder("Key test") then
+			makefolder("Key test")
+		end
+		if not isfolder("Key test/MrMaxNaJa/") then
+			makefolder("Key test/MrMaxNaJa/")
+		end
+		if not isfile("Key test/MrMaxNaJa/?.json") then-- .. game.Players.LocalPlayer.Name .. ".json") then
+			writefile("Key test/MrMaxNaJa/?.json", game:GetService("HttpService"):JSONEncode(_G.Settings))-- .. game.Players.LocalPlayer.Name .. ".json", game:GetService("HttpService"):JSONEncode(_G.Settings))
+		else
+			local L_54_ = game:GetService("HttpService"):JSONDecode(readfile("Key test/MrMaxNaJa/?.json")) --.. game.Players.LocalPlayer.Name .. ".json"))
+			for L_55_forvar0, L_56_forvar1 in pairs(L_54_) do
+				_G.Settings[L_55_forvar0] = L_56_forvar1
+			end
+		end
+	else
+		return warn("Status : Undetected Executor")
+	end
 end
 
 function SaveSettings()
-    if readfile and writefile and isfile and isfolder then
-        if not isfile("Key test/MrMaxNaJa/?.json") then
-            LoadSettings()
-        else
-            local L_57_ = game:GetService("HttpService"):JSONDecode(readfile("Key test/MrMaxNaJa/?.json"))
-            local L_58_ = {}
-            for L_59_forvar0, L_60_forvar1 in pairs(_G.Settings) do
-                L_58_[L_59_forvar0] = L_60_forvar1
-            end
-            writefile("Key test/MrMaxNaJa/?.json", game:GetService("HttpService"):JSONEncode(L_58_))
-        end
-    else
-        return warn("Status : Undetected Executor")
-    end
+	if readfile and writefile and isfile and isfolder then
+		if not isfile("Key test/MrMaxNaJa/?.json") then-- .. game.Players.LocalPlayer.Name .. ".json") then
+			LoadSettings()
+		else
+			local L_57_ = game:GetService("HttpService"):JSONDecode(readfile("Key test/MrMaxNaJa/?.json")) --.. game.Players.LocalPlayer.Name .. ".json"))
+			local L_58_ = {}
+			for L_59_forvar0, L_60_forvar1 in pairs(_G.Settings) do
+				L_58_[L_59_forvar0] = L_60_forvar1
+			end
+			writefile("Key test/MrMaxNaJa/?.json", game:GetService("HttpService"):JSONEncode(L_58_))--" .. game.Players.LocalPlayer.Name .. ".json", game:GetService("HttpService"):JSONEncode(L_58_))
+		end
+	else
+		return warn("Status : Undetected Executor")
+	end
 end
 
-local ClientId = game:GetService("RbxAnalyticsService"):GetClientId() 
-local whitelist = string.split(ClientId, '-') 
-local Hxven = whitelist[5] 
-_G.Hxven = whitelist[5]
+local ClientId = game:GetService("RbxAnalyticsService"):GetClientId() local whitelist = string.split((ClientId), '-') local Hxven = whitelist[5] _G.Hxven = whitelist[5]
 _G.Settings = {
-    ClientId = ClientId
+    --OFF = false,
+    --Hxven = Hxven, --B1786EEC0CF8
+    ClientId = ClientId --game:GetService("RbxAnalyticsService"):GetClientId() --3C716E0A-F381-42CB-A634-B1786EEC0CF8
 }
-
 SaveSettings()
 LoadSettings()
 
 local whitelistPak = {
-    ["Pak1"] = _G.Hxven
+"Pak1",
+"Pak2",
 }
 
 local whitelist = {
-    [_G.Hxven] = "1",
-    [_G.Hxven] = "2"
+[_G.Hxven] = "1",
+[_G.Hxven] = "2",
+--[_G.Hxven] = "3C716E0A-F381-42CB-A634-B1786EEC0CF8",
 }
 
 if _G.antLoop == true then
     _G.antLoop = true
 else
-    if whitelistPak[_G.Hxven] == getgenv().K then
-        print("ขั้นตอนที่ 1: ผ่าน [1]")
+    if getgenv().K ~= whitelistPak then print("ผ่านขั้นตอนที่ 1 ในการพัก [1]")
         if whitelist[_G.Hxven] == game:GetService("RbxAnalyticsService"):GetClientId() then
-            print("ขั้นตอนที่ 2: HWID ผ่าน [1]")
+            print("ผ่านขั้นตอนที่ 2 ในการเช็ค HWID [1]")
             _G.antLoop = true
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Narongrit2007/05ds4fd/refs/heads/main/asdfcxvdfdfs-n-a-j-a.lua"))()
-            print("สคริปต์รันสำเร็จ")
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Narongrit2007/05ds4fd/refs/heads/main/asdfcxvdfdfs-n-a-j-a.lua"))()
+			print("รันสคริป โอเย้")
+        elseif _G.Settings.ClientId == game:GetService("RbxAnalyticsService"):GetClientId() then
+            print("แก้ไขเรียบร้อย")
+            _G.antLoop = true
+            print("ผ่านขั้นตอนที่ 2 ในการเช็ค HWID [2]")
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Narongrit2007/05ds4fd/refs/heads/main/asdfcxvdfdfs-n-a-j-a.lua"))()
+			print("รันสคริป โอเย้")
         else
-            print("ไม่ผ่านขั้นตอนที่ 2: HWID ไม่ถูกต้อง")
-            print("ติดต่อแอดมินด่วน")
+            print("ไม่ผ่านขั้นตอนที่ 2 ในการเช็ค HWID")
+            wait(1)
+            print("มึงเป็นใคร") print("ติดต่อแอดมินด่วน")
         end
         if _G.Settings.OFF == false then
             _G.Settings.Hxven = _G.Hxven
@@ -85,18 +84,48 @@ else
             _G.Settings.OFF = true
             SaveSettings()
         end
-    else
-        print("ไม่พบ Key ในระบบ")
+	elseif whitelistPak == getgenv().K then print("ผ่านขั้นตอนที่ 1 ในการพัก [2]")
+        if whitelist[_G.Hxven] == game:GetService("RbxAnalyticsService"):GetClientId() then
+            print("ผ่านขั้นตอนที่ 2 ในการเช็ค HWID [1]")
+            _G.antLoop = true
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Narongrit2007/05ds4fd/refs/heads/main/asdfcxvdfdfs-n-a-j-a.lua"))()
+			print("รันสคริป โอเย้")
+        elseif _G.Settings.ClientId == game:GetService("RbxAnalyticsService"):GetClientId() then
+            print("แก้ไขเรียบร้อย")
+            _G.antLoop = true
+            print("ผ่านขั้นตอนที่ 2 ในการเช็ค HWID [2]")
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Narongrit2007/05ds4fd/refs/heads/main/asdfcxvdfdfs-n-a-j-a.lua"))()
+			print("รันสคริป โอเย้")
+        else
+            print("ไม่ผ่านขั้นตอนที่ 2 ในการเช็ค HWID")
+            wait(1)
+            print("มึงเป็นใคร") print("ติดต่อแอดมินด่วน")
+        end
+        if _G.Settings.OFF == false then
+            _G.Settings.Hxven = _G.Hxven
+            _G.Settings.ClientId = game:GetService("RbxAnalyticsService"):GetClientId()
+            _G.Settings.K = getgenv().K
+            _G.Settings.OFF = true
+            SaveSettings()
+        end
+    else print("ไม่ผ่านขั้นตอนที่ 1 ไม่พบ Key ในระบบ กำลังเช็คให้ละเอียด")
         if getgenv().K == _G.Settings.K then
             _G.Settings.OFF = true
             SaveSettings()
             print("ติดต่อแอดมิน")
-        else
-            getgenv().K = _G.Settings.K
+		else
+			getgenv().K = _G.Settings.K
+        end
+        if _G.Settings.OFF == false then
+            _G.Settings.Hxven = _G.Hxven
+            _G.Settings.ClientId = game:GetService("RbxAnalyticsService"):GetClientId()
+            _G.Settings.K = getgenv().K
+            _G.Settings.OFF = true
+            SaveSettings()
+            print("แก้ไขเรียบร้อย ลงรันใหม่อีกครั้ง")
         end
     end
 end
-
 
 if _G.Settings.OFF == false then
 	local url = "https://discordapp.com/api/webhooks/1299748672434868224/w6B9RODJLf1jCoN0Tzbziy8FrPfMzDgq44q9q3JjwppYBHc7BYsVfPazcmgVUF-OZAq8" -- ur webhook url
