b88ee19e888af386a05f7126bb28573baf53f565
Narongrit2007 <bakmaxnajabakmaxnaja@gmail.com>
2025-03-24 22:46:31 +0700

    Update HHHHHHHHH-dsfsaf-N---a--j--a--.lua

--- HHHHHHHHH-dsfsaf-N---a--j--a--.lua
+++ HHHHHHHHH-dsfsaf-N---a--j--a--.lua
@@ -1,4 +1,4 @@
 local whitelistPak = {
-    { Hwid = "53FF23FF-8908-4496-8B86-147B02015C9D", Key = "Pak1", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
+    { Hwid = "845F3607-8A0E-4D9A-9E13-392E4BD64248", Key = "Pak1", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
     --{ Hwid = "53FF23FF-8908-4496-8B86-147B02015C9D", Key = "Pak2", ExpiryDate = math.huge, Permanent = true } -- Key แบบถาวร
 }
