225f9fa26a36a60be3eb337cec7ef101b0450b03
Narongrit2007 <bakmaxnajabakmaxnaja@gmail.com>
2025-03-24 23:32:17 +0700

    Update HHHHHHHHH-dsfsaf-N---a--j--a--.lua

--- HHHHHHHHH-dsfsaf-N---a--j--a--.lua
+++ HHHHHHHHH-dsfsaf-N---a--j--a--.lua
@@ -1,4 +1,38 @@
-local whitelistPak = {
-    { Hwid = "845F3607-8A0E-4D9A-9E13-392E4BD64248", Key = "Pak1", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
-    --{ Hwid = "53FF23FF-8908-4496-8B86-147B02015C9D", Key = "Pak2", ExpiryDate = math.huge, Permanent = true } -- Key แบบถาวร
+local whitelistPak = {--845F3607-8A0E-4D9A-9E13-392E4BD64248
+    { Hwid = "", Key = "2OBCL-WCZLY-0FE0F-1JBO2", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
+    { Hwid = "", Key = "M7TJG-B7LZD-5Y4TI-A8R09", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
+    { Hwid = "", Key = "HH617-QVFGC-W5KTC-VWDO7", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
+    { Hwid = "", Key = "051E7-TVC4F-FL50S-7URWP", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
+    
+    { Hwid = "", Key = "5ISYD-9VTM4-CQGAW-FZPN8", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "", Key = "C7WU3-AT3M4-4E3TN-ROWWI", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "", Key = "9KDJV-K5H5Z-U40VV-OCKMS", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "", Key = "O7KOU-0MYM5-50JEE-VHH90", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "", Key = "UJLHS-QUC6L-WB6KY-61LE2", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "", Key = "H1RFD-87SS9-YJ50Z-TKYLZ", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "", Key = "F3K03-KQA13-L9F8C-SNT4W", ExpiryDate = math.huge, Permanent = true } -- Key แบบถาวร
 }
+--[[
+---- kEY เซ่า ----
+\/ \/ \/ \/ \/ \/
+2OBCL-WCZLY-0FE0F-1JBO2
+M7TJG-B7LZD-5Y4TI-A8R09
+HH617-QVFGC-W5KTC-VWDO7
+051E7-TVC4F-FL50S-7URWP
+
+BOTNC-PP50P-8C6JH-GJBDC
+1Q3FU-P6MAO-I0MJV-Q7I60
+8KMBS-CBDQZ-P3RP9-MC4SL
+QLW3T-F2UEH-L877I-P8NVA
+5AHKQ-8ZDOI-AQLSW-YIOH3
+9I84K-9TIJZ-40ZZL-4K89Y
+----- key ถาวร -----
+5ISYD-9VTM4-CQGAW-FZPN8
+C7WU3-AT3M4-4E3TN-ROWWI
+9KDJV-K5H5Z-U40VV-OCKMS
+O7KOU-0MYM5-50JEE-VHH90
+UJLHS-QUC6L-WB6KY-61LE2
+H1RFD-87SS9-YJ50Z-TKYLZ
+F3K03-KQA13-L9F8C-SNT4W
+
+]]
