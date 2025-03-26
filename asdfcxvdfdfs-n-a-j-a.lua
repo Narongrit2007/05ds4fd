f0a46061238507717094c9fb77cb1eb764151019
Narongrit2007 <bakmaxnajabakmaxnaja@gmail.com>
2025-03-27 00:34:53 +0700

    Update asdfcxvdfdfs-n-a-j-a.lua

--- asdfcxvdfdfs-n-a-j-a.lua
+++ asdfcxvdfdfs-n-a-j-a.lua
@@ -27,18 +27,18 @@ _G.Antloopm = false
 --loadstring(game:HttpGet("https://pastebin.com/raw/m3QzDi35"))()
 -- รายการคีย์และวันหมดอายุ
 local whitelistPak = {--845F3607-8A0E-4D9A-9E13-392E4BD64248
-    { Hwid = "af72c7c3-e9b1-498e-a4e8", Key = "", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
-    { Hwid = "", Key = "M7TJG-B7LZD-5Y4TI-A8R09", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
-    { Hwid = "", Key = "HH617-QVFGC-W5KTC-VWDO7", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
-    { Hwid = "", Key = "051E7-TVC4F-FL50S-7URWP", ExpiryDate = os.time{year=0, month=0, day=0, hour=0, min=0}, Permanent = false },
+    { Hwid = "af72c7c3-e9b1-498e-a4e8", Key = "Key", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
+    { Hwid = "M7TJG", Key = "M7TJG-B7LZD-5Y4TI-A8R09", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
+    { Hwid = "HH617", Key = "HH617-QVFGC-W5KTC-VWDO7", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
+    { Hwid = "051E7", Key = "051E7-TVC4F-FL50S-7URWP", ExpiryDate = os.time{year=2025, month=11, day=25, hour=24, min=0}, Permanent = false },
     
     { Hwid = "3153E1FF-1F63-438F-B33E-CAE700815AD3", Key = "5ISYD-9VTM4-CQGAW-FZPN8", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
     { Hwid = "af72c7c3-e9b1-498e-a4e8-135ba777df70", Key = "C7WU3-AT3M4-4E3TN-ROWWI", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
-    { Hwid = "", Key = "9KDJV-K5H5Z-U40VV-OCKMS", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
-    { Hwid = "", Key = "O7KOU-0MYM5-50JEE-VHH90", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
-    { Hwid = "", Key = "UJLHS-QUC6L-WB6KY-61LE2", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
-    { Hwid = "", Key = "H1RFD-87SS9-YJ50Z-TKYLZ", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
-    { Hwid = "", Key = "F3K03-KQA13-L9F8C-SNT4W", ExpiryDate = math.huge, Permanent = true } -- Key แบบถาวร
+    { Hwid = "9KDJV", Key = "9KDJV-K5H5Z-U40VV-OCKMS", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "O7KOU", Key = "O7KOU-0MYM5-50JEE-VHH90", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "UJLHS", Key = "UJLHS-QUC6L-WB6KY-61LE2", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "H1RFD", Key = "H1RFD-87SS9-YJ50Z-TKYLZ", ExpiryDate = math.huge, Permanent = true }, -- Key แบบถาวร
+    { Hwid = "F3K03", Key = "F3K03-KQA13-L9F8C-SNT4W", ExpiryDate = math.huge, Permanent = true } -- Key แบบถาวร
 }
 --[[
 ---- kEY เซ่า ----
